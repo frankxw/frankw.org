c4ae5a1150423a6c491cfba1d16dc85ff8588929
Frank Williams <demalus@gmail.com>
2016-03-08 15:16:23 -0500

    Initial commit with some basic html, css, and testing in place

---
+++ test/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express'),
+    path    = require('path');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.use(express.static('..'));
+
+app.listen(8080);
